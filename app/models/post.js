ea4326535ab8f4d7483b1e52f0399b3dae034942
Jonathan Jackson <jonathan.jackson1@me.com>
2015-04-28 17:14:50 -0400

    Add post model
    Fixes the "it can be created with title / body / authorName" test

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  // Specify attributes with DS.attr()
+  title: DS.attr('string'),
+  body: DS.attr('string'),
+  authorName: DS.attr('string')
 });
