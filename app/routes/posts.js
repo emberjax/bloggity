f3bfbb9c43a34266d9869704d71b5d587ec03b70
Jonathan Jackson <jonathan.jackson1@me.com>
2015-04-28 17:16:28 -0400

    Add post route
    Fixes the "model hook retrieves from posts api endpoint" test

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // Implement this routes' model hook see more information here:
-  // http://guides.emberjs.com/v1.10.0/routing/specifying-a-routes-model/
+  model: function(){
+    return this.store.find('post');
+  }
 });
