d7178982ea3596de6851d172f9b33ae657b99e9e
Jonathan Jackson <jonathan.jackson1@me.com>
2015-04-28 12:54:24 -0400

    Add routes:posts with tip

---
+++ app/routes/posts.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // Implement this routes' model hook see more information here:
+  // http://guides.emberjs.com/v1.10.0/routing/specifying-a-routes-model/
+});
