f38adaedeb05a77f11c80e369772608b1190834b
Shaun  Hubbard <shaun.hubbard@ascentis.com>
2015-04-30 20:23:47 -0400

    Ember Bloggity with acceptance tests for Index and Back button

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  // Implement namespace here see more information here:
-  // http://guides.emberjs.com/v1.11.0/models/customizing-adapters/
+  namespace: 'api'
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // Implement this routes model hook for more information see:
-  // http://guides.emberjs.com/v1.10.0/routing/specifying-a-routes-model/
+  model: function(params){
+    return this.store.find('post', params["post_id"]);
+  }
 });
