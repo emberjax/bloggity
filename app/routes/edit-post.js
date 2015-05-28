8a59d9c3f2783e4378390a6f8839bbfdb362c8e6
Jonathan Jackson <jonathan.jackson1@me.com>
2015-05-28 18:21:43 -0400

    Ensure model is torn down on non-submit add back button

--- app/routes/edit-post.js
+++ app/routes/edit-post.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.find('post', params.id);
+  },
+  actions: {
+    willTransition(){
+      this.get('currentModel').rollback();
+    }
   }
 });
